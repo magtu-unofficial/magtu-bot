af9f83519ad0d959a11cfe8e5ee1c1ce49ba5fed
ivanik <ivan170102@gmail.com>
2019-02-26 12:07:28 +0500

    Добавил текст помощи

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Router from "./router";
 
 import timetable from "./timetable";
+import help from "./help";
 
 const router = new Router();
 
 router.add("timetable", timetable);
+router.add("help", help);
 
 export default router.route;
