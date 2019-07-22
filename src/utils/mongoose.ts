8c17c5d2864c2a861b8cce9f5c0f747f02f86466
ivanik <ivan170102@gmail.com>
2019-07-22 18:21:54 +0500

    Исправленно подключение к mongodb

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,7 +1,22 @@
 import mongoose from "mongoose";
 
+import log from "./log";
 import { mongoUri } from "./config";
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(
+  mongoUri,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500
+  },
+  err => {
+    if (err) {
+      log.error(err.message);
+      process.exit(-1);
+    }
+  }
+);
 
 export default mongoose;
