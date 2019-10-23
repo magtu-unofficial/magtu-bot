31421e308c3bf489061b674deb5d81b3908b8574
ivanik <ivan170102@gmail.com>
2019-10-23 18:31:23 +0500

    Посхал.очка

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,7 +2,15 @@ import help from "./help";
 import timetable from "./timetable";
 import { timetableCmd } from "../text";
 
+import tt from "../reply/timetable";
+import date from "../utils/date";
+import sb from "../interfaces/subgroup";
+
 export default (bot: any) => {
   bot.command(/справка|помощь/, help);
   bot.command(timetableCmd, timetable);
+  bot.command(/рейд/i, async ctx => {
+    await ctx.send("Э, Рейд, лови расписуху на завтра");
+    tt(ctx, date("завтра"), "исп-19-4", sb.second);
+  });
 };
